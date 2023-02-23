53b6321395137ec419ed88a95de0e2c29777cd5b
muszynski <muszynski@gmail.com>
2023-02-23 19:44:36 +0100

    coreData

---
+++ learningGPT/learningGPT/Data/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var persistentContainer: NSPersistentContainer?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Utwórz persistent container
+        persistentContainer = NSPersistentContainer(name: "Model")
+        persistentContainer?.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Nie udało się załadować magazynu trwałego: \(error)")
+            }
+        })
+
+        return true
+    }
+
+    // Zwróć kontekst Core Data
+    func getContext() -> NSManagedObjectContext {
+        return persistentContainer!.viewContext
+    }
+}
