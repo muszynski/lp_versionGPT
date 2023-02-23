96bb391cecd43b0bfe2e3614835042424330a7d2
muszynski <muszynski@gmail.com>
2023-02-23 20:29:07 +0100

    data

--- learningGPT/learningGPT/Data/AppDelegate.swift
+++ learningGPT/learningGPT/Data/AppDelegate.swift
@@ -1,24 +1,45 @@
 import UIKit
 import CoreData
 
-class AppDelegate: UIResponder, UIApplicationDelegate {
+var persistentContainer: NSPersistentContainer = {
+    /*
+     The persistent container for the application. This implementation
+     creates and returns a container, having loaded the store for the
+     application to it. This property is optional since there are legitimate
+     error conditions that could cause the creation of the store to fail.
+    */
+    let container = NSPersistentContainer(name: "Model")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        if let error = error as NSError? {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 
-    var persistentContainer: NSPersistentContainer?
+            /*
+             Typical reasons for an error here include:
+             * The parent directory does not exist, cannot be created, or disallows writing.
+             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+             * The device is out of space.
+             * The store could not be migrated to the current model version.
+             Check the error message to determine what the actual problem was.
+             */
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+    })
+    return container
+}()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Utwórz persistent container
-        persistentContainer = NSPersistentContainer(name: "Model")
-        persistentContainer?.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error {
-                fatalError("Nie udało się załadować magazynu trwałego: \(error)")
-            }
-        })
+// MARK: - Core Data Saving support
 
-        return true
-    }
-
-    // Zwróć kontekst Core Data
-    func getContext() -> NSManagedObjectContext {
-        return persistentContainer!.viewContext
+func saveContext() {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+        do {
+            try context.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
     }
 }
--- learningGPT/learningGPT/View/AddLesson.swift
+++ learningGPT/learningGPT/View/AddLesson.swift
@@ -6,15 +6,63 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AddLesson: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var content = ""
+    @State private var subject = ""
+    @State private var reminderDate = Date()
+
     var body: some View {
-        Text("Add lesson")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .foregroundColor(.blue)
+        NavigationView {
+            Form {
+                Section(header: Text("Content")) {
+                    TextEditor(text: $content)
+                }
+                
+                Section(header: Text("Subject")) {
+                    TextField("Subject", text: $subject)
+                }
+                
+                Section(header: Text("Reminder Date")) {
+                    DatePicker("Select a date", selection: $reminderDate, displayedComponents: [.date, .hourAndMinute])
+                }
+            }
+            .navigationTitle("Add Learning Object")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        addLearningObject()
+                    }
+                }
+            }
+        }
+    }
+    
+    private func addLearningObject() {
+        withAnimation {
+            let newLearningObject = LearningObject(context: viewContext)
+            newLearningObject.addDate = Date()
+            newLearningObject.modificationDate = Date()
+            newLearningObject.content = content
+            newLearningObject.subject = subject
+            newLearningObject.reminderDate = reminderDate
+            newLearningObject.status = "New"
+            
+            do {
+                try viewContext.save()
+                presentationMode.wrappedValue.dismiss()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
+
 
 struct AddLesson_Previews: PreviewProvider {
     static var previews: some View {
