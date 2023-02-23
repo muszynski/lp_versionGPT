d74f8978cc05ac8deb0085a9523b06b5ca05ee1f
muszynski <muszynski@gmail.com>
2023-02-23 19:25:34 +0100

    init

---
+++ learningGPT/learningGPT/learningGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGPTApp.swift
+//  learningGPT
+//
+//  Created by Łukasz Muszyński on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct learningGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
